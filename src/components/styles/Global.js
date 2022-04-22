d4096f598b679c57ee233e234afb54742749f919
Gonzalo <gonzalopiombi@gmail.com>
2022-04-22 16:02:00 -0300

    Create Global styles.

---
+++ src/components/styles/Global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box
+  }
+
+  body {
+    background-color: #dae0e6;
+    margin: 0    
+  }
+`;
+
+export default GlobalStyles;
