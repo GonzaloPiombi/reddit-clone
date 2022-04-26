ccaa2427ae29ebd183d54fc1ae29a5d5c2ff55cc
Gonzalo <gonzalopiombi@gmail.com>
2022-04-26 12:06:53 -0300

    Add buttons to the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { Button, AltButton, ButtonContainer } from './styles/Button.styled';
 
 const Header = () => {
   return (
     <StyledHeader>
       <Nav>
         <Logo src="./images/reddit-logo.png" alt="logo" />
+        <ButtonContainer>
+          <AltButton>Log In</AltButton>
+          <Button>Sign Up</Button>
+        </ButtonContainer>
       </Nav>
     </StyledHeader>
   );
