5ff52c9ef544dc60719b581c49f33141471554f4
Gonzalo <gonzalopiombi@gmail.com>
2022-04-26 12:05:52 -0300

    Create styles for the buttons.

---
+++ src/components/styles/Button.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #0079d3;
+  border: 1px solid #0079d3;
+  border-radius: 9999px;
+  color: #fff;
+  cursor: pointer;
+  font-size: 1rem;
+  min-width: 120px;
+  padding: 6px 30px;
+
+  &:hover {
+    background-color: #1484d7;
+  }
+
+  &:active {
+    background-color: #3d99de;
+  }
+`;
+
+const AltButton = styled(Button)`
+  background-color: #fff;
+  color: #0079d3;
+
+  &:hover {
+    background-color: #f5fafe;
+  }
+
+  &:active {
+    background-color: #d7eaf8;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-width: 300px;
+`;
+
+export { Button, AltButton, ButtonContainer };
