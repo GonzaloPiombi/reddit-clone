4f2ddb6d9260550b540754d868dc55e4c4bc8e43
Gonzalo <gonzalopiombi@gmail.com>
2022-04-27 12:36:28 -0300

    Create function that fires on first render and queries all the posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import GlobalStyles from './components/styles/Global';
 import Header from './components/Header';
+import {
+  getFirestore,
+  collectionGroup,
+  getDocs,
+  getDoc,
+  doc,
+} from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import Card from './components/Card';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const db = getFirestore();
+        //Get every collection with the 'posts' name.
+        const colRef = collectionGroup(db, 'posts');
+
+        const snapshot = await getDocs(colRef);
+        let data = [];
+        await Promise.all(
+          snapshot.docs.map(async (item) => {
+            //Query the name of the subreddit the post was made and add it to the object.
+            const docSnapshot = await getDoc(
+              doc(db, 'subs', item.ref.parent.parent.id)
+            );
+            const subName = docSnapshot.data().name;
+            data.push({ ...item.data(), id: item.id, subName: subName });
+          })
+        );
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getPosts();
+  }, []);
+
   return (
     <div className="App">
       <GlobalStyles />
       <Header />
+      <Card posts={posts} />
     </div>
   );
 }
