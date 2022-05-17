f7e968f565f7056f7a6a2b105ea25b3cf2f8156c
Gonzalo <gonzalopiombi@gmail.com>
2022-05-17 11:35:22 -0300

    Separate function that changes username from the sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,29 +1,42 @@
+import { useState } from 'react';
 import { Button } from './styles/Button.styled';
 import Modal from './styles/Modal';
 import { StyledSignInUp } from './styles/SignInUp.styled';
 import { createUserWithEmailAndPassword, updateProfile } from '@firebase/auth';
 
 const SignUp = (props) => {
+  const [isSignedUp, setIsSignedUp] = useState(false);
+
   const signUp = async (e) => {
     try {
       e.preventDefault();
       const email = e.target.email.value;
       const password = e.target.password.value;
-      const username = e.target.username.value;
       const userCred = await createUserWithEmailAndPassword(
         props.auth,
         email,
         password
       );
       e.target.reset();
+
+      //Use anonymouse as a placeholder and display it if user does not select a username after.
       await updateProfile(userCred.user, {
-        displayName: username,
+        displayName: 'anonymous',
       });
-      console.log(userCred);
-      props.signIn(email, password);
+      setIsSignedUp(true);
     } catch (error) {
       console.log(error.message);
     }
+  };
+
+  const changeUsername = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const user = props.auth.currentUser;
+    await updateProfile(user, {
+      displayName: username,
+    });
+    props.signIn();
   };
 
   return (
@@ -34,20 +47,29 @@ const SignUp = (props) => {
         </div>
         <div>
           <h3>Sign up</h3>
-          <form onSubmit={signUp}>
-            <label htmlFor="username">CHOOSE A USERNAME</label>
-            <input required type="text" id="username" name="username" />
-            <label htmlFor="email">EMAIL</label>
-            <input required type="email" id="email" name="email" />
-            <label htmlFor="password">PASSWORD</label>
-            <input required type="password" id="password" name="password" />
-            <Button type="submit">Sign Up</Button>
-          </form>
+          {!isSignedUp && (
+            <form onSubmit={signUp}>
+              <label htmlFor="email">EMAIL</label>
+              <input required type="email" id="email" name="email" />
+              <label htmlFor="password">PASSWORD</label>
+              <input required type="password" id="password" name="password" />
+              <Button type="submit">Sign Up</Button>
+            </form>
+          )}
+          {isSignedUp && (
+            <form onSubmit={changeUsername}>
+              <label htmlFor="username">CHOOSE A USERNAME</label>
+              <input required type="text" id="username" name="username" />
+              <Button type="submit">Sign Up</Button>
+            </form>
+          )}
         </div>
         <div>
-          <button onClick={props.showSignUpForm} className="close-button">
-            <i className="las la-times"></i>
-          </button>
+          {!isSignedUp && (
+            <button onClick={props.showSignUpForm} className="close-button">
+              <i className="las la-times"></i>
+            </button>
+          )}
         </div>
       </StyledSignInUp>
     </Modal>
