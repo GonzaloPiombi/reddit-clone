ca09bf2d8540bf909dda38cba2c9623e3a650e31
Gonzalo <gonzalopiombi@gmail.com>
2022-05-23 15:45:53 -0300

    Create styled-components for inputs and label

---
+++ src/components/styles/Input.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: #fcfcfb;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  font-size: 1rem;
+  height: 40px;
+  margin-bottom: 10px;
+  outline: none;
+`;
+
+const Label = styled.label`
+  color: #525252;
+  font-size: 0.9rem;
+`;
+
+export { Input, Label };
