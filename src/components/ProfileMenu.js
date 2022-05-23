ac39435a86ddf306fde5ca990a552bbf95756e82
Gonzalo <gonzalopiombi@gmail.com>
2022-05-23 16:30:19 -0300

    Add react router link

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -1,11 +1,21 @@
 import { StyledProfileMenu } from './styles/Profile.styled';
+import { Link } from 'react-router-dom';
 
 const ProfileMenu = () => {
   return (
     <StyledProfileMenu>
       <button>
-        <i className="las la-user-circle"></i>
-        <p>Profile</p>
+        <Link
+          to="/profile"
+          style={{
+            textDecoration: 'none',
+            color: 'inherit',
+            display: 'inherit',
+          }}
+        >
+          <i className="las la-user-circle"></i>
+          <p>Profile</p>
+        </Link>
       </button>
       <button>
         <i className="las la-sign-out-alt"></i>
