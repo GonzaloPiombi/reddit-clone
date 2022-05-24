f9f57e4ba2b3f33a097eff86616aa04fd8332b4d
Gonzalo <gonzalopiombi@gmail.com>
2022-05-24 11:36:28 -0300

    Add function that changes username

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,37 @@
+import { useState } from 'react';
 import { StyledProfile } from './styles/Profile.styled';
 import { Form, Input, Label } from './styles/Form.styled';
 import { Button } from './styles/Button.styled';
+import { changeUsername } from '../helpers/helpers';
+import Loader from './Loader';
+import { useNavigate } from 'react-router';
 
-const Profile = () => {
+const Profile = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const setUsername = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const hasUsernameChanged = await changeUsername(
+      props.auth.currentUser,
+      e.target.username.value,
+      e.target
+    );
+    if (!hasUsernameChanged) {
+      setIsLoading(false);
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
     <StyledProfile>
       <h3>User Settings</h3>
-      <Form>
-        <Label>CHANGE YOUR USERNAME</Label>
-        <Input type="text" />
-        <Button type="submit">Save</Button>
+      <Form onSubmit={setUsername}>
+        <Label htmlFor="username">CHANGE YOUR USERNAME</Label>
+        <Input type="text" id="username" name="username" />
+        {!isLoading ? <Button type="submit">Save</Button> : <Loader />}
       </Form>
     </StyledProfile>
   );
