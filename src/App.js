8b712adda9b882b847464addd1ab4b63f0ab9680
Gonzalo <gonzalopiombi@gmail.com>
2022-05-24 11:36:46 -0300

    Pass auth as prop to profile

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ function App() {
               </>
             }
           />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<Profile auth={auth} />} />
         </Routes>
         {signUp && (
           <SignUp
