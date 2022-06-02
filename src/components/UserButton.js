eae21d96f94423bf6723a9ccf50204bd27394b3d
Gonzalo <gonzalopiombi@gmail.com>
2022-06-02 15:04:34 -0300

    Change imports

--- src/components/UserButton.js
+++ src/components/UserButton.js
@@ -1,12 +1,12 @@
-import { StyledUserButton } from './styles/UserButton.styled';
+import { StyledMenuButton } from './styles/MenuButton.styled';
 
 const UserButton = (props) => {
   return (
-    <StyledUserButton onClick={props.onButtonClick}>
+    <StyledMenuButton onClick={props.onButtonClick}>
       <img src="./images/pp.png" alt="Default avatar" />
       <p>{props.user}</p>
       <i className="las la-angle-down"></i>
-    </StyledUserButton>
+    </StyledMenuButton>
   );
 };
 
