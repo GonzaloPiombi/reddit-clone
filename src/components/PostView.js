19c53f308582b2c5ce7a62606f49ce245913e7f0
Gonzalo <gonzalopiombi@gmail.com>
2022-06-08 16:51:47 -0300

    Create component that shows the selected post

---
+++ src/components/PostView.js
@@ -0,0 +1,20 @@
+import CardSidebar from './CardSidebar';
+import CardTop from './CardTop';
+import CardBottom from './CardBottom';
+import { PostCard } from './styles/Card.styled';
+
+const PostView = ({ post }) => {
+  return (
+    <div style={{ marginTop: '75px' }}>
+      <PostCard>
+        <CardSidebar votes={post.votes} />
+        <CardTop author={post.author} date={post.date} subName={post.subName} />
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
+        <CardBottom />
+      </PostCard>
+    </div>
+  );
+};
+
+export default PostView;
