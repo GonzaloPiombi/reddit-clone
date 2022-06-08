64e17094f68aa2a4ace42f153d89a75a563f8ec6
Gonzalo <gonzalopiombi@gmail.com>
2022-06-08 10:37:33 -0300

    Add Links that redirects to the post

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import CardSidebar from './CardSidebar';
 import CardTop from './CardTop';
 import CardBottom from './CardBottom';
 import { StyledCard } from './styles/Card.styled';
+import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   return props.posts.map(
@@ -9,10 +10,15 @@ const Card = (props) => {
       return (
         <StyledCard key={id}>
           <CardSidebar votes={votes} />
-          <CardTop author={author} date={date} subName={subName} />
-          <h2>{title}</h2>
-          <p>{content}</p>
-          <CardBottom />
+          <Link
+            to={`/r/${subName}/${id}`}
+            style={{ textDecoration: 'none', color: '#000' }}
+          >
+            <CardTop author={author} date={date} subName={subName} />
+            <h2>{title}</h2>
+            <p>{content}</p>
+            <CardBottom />
+          </Link>
         </StyledCard>
       );
     }
