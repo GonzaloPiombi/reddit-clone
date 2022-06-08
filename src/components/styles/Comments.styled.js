6ae6be48ce456601406d320acca717a629ae7fe7
Gonzalo <gonzalopiombi@gmail.com>
2022-06-08 16:53:11 -0300

    Add styles for the comment section

---
+++ src/components/styles/Comments.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const StyledCommentSection = styled.div`
+  background-color: #fff;
+  margin: 0 auto;
+  width: 650px;
+`;
+
+const StyledComment = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CommentTop = styled.div`
+  display: flex;
+`;
+
+export { StyledCommentSection, StyledComment, CommentTop };
