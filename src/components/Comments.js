42c05495937bd57f2e85911cc1302006a96a1c2f
Gonzalo <gonzalopiombi@gmail.com>
2022-06-09 11:34:17 -0300

    Render the children of a comment

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,29 +1,42 @@
 import {
   StyledCommentSection,
   StyledComment,
-  CommentTop,
+  StyledCommentBottom,
+  Replies,
 } from './styles/Comments.styled';
+import { StyledCardTop } from './styles/Card.styled';
 import { formatDate } from '../helpers/helpers';
 
 const Comments = ({ comments }) => {
-  console.log(comments);
+  const renderReplies = (replies) => {
+    return <Comments comments={replies} />;
+  };
+
   return (
     <StyledCommentSection>
       {comments.map((comment) => {
         return (
-          <StyledComment>
-            <CommentTop>
-              <h5>{comment.author}</h5>
-              <span>.</span>
-              <p>{formatDate(comment.date.toDate())}</p>
-            </CommentTop>
+          <StyledComment key={comment.id}>
             <div>
-              <p>{comment.content}</p>
+              <StyledCardTop>
+                <h5>{comment.author}</h5>
+                <div>
+                  <span>.</span>
+                </div>
+                <p>{formatDate(comment.date.toDate())}</p>
+              </StyledCardTop>
+              <div>
+                <p>{comment.content}</p>
+              </div>
+              <StyledCommentBottom>
+                <p>{comment.votes}</p>
+                <button>Reply</button>
+              </StyledCommentBottom>
             </div>
-            <div>
-              <p>{comment.votes}</p>
-              <button>Reply</button>
-            </div>
+
+            {comment.replies.length > 0 ? (
+              <Replies>{renderReplies(comment.replies)}</Replies>
+            ) : null}
           </StyledComment>
         );
       })}
