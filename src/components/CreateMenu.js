6dc41c9c55149912a08cd683ec682b4b415e6104
Gonzalo <gonzalopiombi@gmail.com>
2022-06-14 12:01:39 -0300

    Add Link to CreatePost component

--- src/components/CreateMenu.js
+++ src/components/CreateMenu.js
@@ -1,9 +1,22 @@
 import { StyledProfileMenu } from './styles/Profile.styled';
+import { Link } from 'react-router-dom';
 
 const CreateMenu = ({ toggleCreateSub }) => {
   return (
     <StyledProfileMenu style={{ right: '40px' }}>
-      <button>Create Post</button>
+      <button>
+        <Link
+          to="/submit"
+          style={{
+            textDecoration: 'none',
+            color: 'inherit',
+            display: 'inherit',
+            width: '100%',
+          }}
+        >
+          Create Post
+        </Link>
+      </button>
       <button onClick={toggleCreateSub}>Create Subreddit</button>
     </StyledProfileMenu>
   );
