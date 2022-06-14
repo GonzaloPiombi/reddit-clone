03c1d51f26bc14343058f592136d3291448ee5f5
Gonzalo <gonzalopiombi@gmail.com>
2022-06-14 16:04:23 -0300

    Create function that writes the post information into de db

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,56 @@
 import { StyledCreatePost } from './styles/CreatePost.styled';
 import { Select } from './styles/Form.styled';
+import { Button } from './styles/Button.styled';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from '@firebase/firestore';
+import Loader from './Loader';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
-const CreatePost = ({ subList }) => {
+const CreatePost = ({ subList, auth }) => {
+  let navigate = useNavigate();
+  const [isSubmiting, toggleIsSubmitting] = useState(false);
+
+  const submitPost = async (e) => {
+    try {
+      e.preventDefault();
+      toggleIsSubmitting(true);
+      const index = e.target.sublist.selectedIndex;
+      const id = e.target.sublist[index].id;
+      const db = getFirestore();
+      const colRef = collection(db, 'subs');
+      const subColRef = collection(colRef, id, 'posts');
+      console.log(e.target.text.value);
+
+      const docRef = await addDoc(subColRef, {
+        author: auth.currentUser.displayName,
+        title: e.target.title.value,
+        content: e.target.text.value,
+        votes: 0,
+        date: serverTimestamp(),
+      });
+      console.log(docRef);
+      // navigate(`../${e.target.sublist.value}/${docRef.id}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <StyledCreatePost>
       <h2>Create a post</h2>
-      <form>
+      <form onSubmit={submitPost}>
         <div>
           <label>Choose a community</label>
-          <Select required>
+          <Select name="sublist" required>
             {subList.map((sub) => (
-              <option>r/{sub.name}</option>
+              <option key={sub.id} id={sub.id}>
+                r/{sub.name}
+              </option>
             ))}
           </Select>
         </div>
@@ -18,6 +58,7 @@ const CreatePost = ({ subList }) => {
           <input name="title" type="text" placeholder="Title" required />
           <textarea name="text" type="text" placeholder="Text"></textarea>
         </div>
+        {isSubmiting ? <Loader /> : <Button type="submit">Post</Button>}
       </form>
     </StyledCreatePost>
   );
