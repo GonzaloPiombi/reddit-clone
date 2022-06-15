355bcd5dfb3ea92054a719eebb2c53a6c8a39f17
Gonzalo <gonzalopiombi@gmail.com>
2022-06-15 12:27:46 -0300

    Fix bug where redirecting to post would leave postInfo undefined and refactor code

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,50 +1,79 @@
 import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router';
-import { getFirestore, collection, getDocs } from '@firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+} from '@firebase/firestore';
 import PostView from './PostView';
 import Comments from './Comments';
 
 const Post = (props) => {
   const params = useParams();
-  const postInfo = useLocation().state;
+  const info = useLocation().state;
+  const [postInfo, setPostInfo] = useState({});
   const [comments, setComments] = useState([]);
 
+  const findSubredditID = async (colRef) => {
+    const snapshot = await getDocs(colRef);
+    const subreddit = snapshot.docs.find((doc) => {
+      return doc.data().name === params.subreddit;
+    });
+    return subreddit.id;
+  };
+
+  const getPost = async (docRef) => {
+    const post = await getDoc(docRef);
+    setPostInfo(() => {
+      return {
+        ...post.data(),
+        id: post.id,
+        date: post.data().date.toDate(),
+      };
+    });
+  };
+
   useEffect(() => {
-    const getPost = async () => {
-      try {
-        const db = getFirestore();
-        const colRef = collection(db, 'subs');
-        const snapshot = await getDocs(colRef);
-        const subreddit = snapshot.docs.find((doc) => {
-          return doc.data().name === params.subreddit;
-        });
-
-        const postRef = collection(
-          colRef,
-          subreddit.id,
-          'posts',
-          params.id,
-          'comments'
-        );
-
-        const commentsSnapshot = await getDocs(postRef);
-        let postData = [];
-        await Promise.all(
-          commentsSnapshot.docs.map(async (doc) => {
-            let replies = await getReplies(db, doc.ref.path);
-            postData.push({ ...doc.data(), id: doc.id, replies: replies });
-          })
-        );
-        console.log(postData);
-        setComments(postData);
-      } catch (error) {
-        console.log(error.message);
+    const db = getFirestore();
+    const colRef = collection(db, 'subs');
+    const getPostAndComments = async () => {
+      //Get the subreddit id to make the queries for the post and comments.
+      const subredditID = await findSubredditID(colRef);
+      const docRef = doc(colRef, subredditID, 'posts', params.id);
+      //Check if we are coming from the Card component or redirecting from creating a post and setPostInfo accordingly.
+      if (info) {
+        setPostInfo(() => info);
+      } else {
+        await getPost(docRef);
+        console.log(docRef);
       }
+      //Get the comments and replies of the post.
+      await getComments(db, docRef);
     };
 
+    getPostAndComments();
     props.setSub(params.subreddit);
-    getPost();
   }, []);
+
+  const getComments = async (db, docRef) => {
+    try {
+      const postRef = collection(docRef, 'comments');
+
+      const commentsSnapshot = await getDocs(postRef);
+      let postData = [];
+      await Promise.all(
+        commentsSnapshot.docs.map(async (doc) => {
+          let replies = await getReplies(db, doc.ref.path);
+          postData.push({ ...doc.data(), id: doc.id, replies: replies });
+        })
+      );
+      setComments(postData);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const getReplies = async (db, path) => {
     let subComments = [];
@@ -59,6 +88,7 @@ const Post = (props) => {
     );
     return subComments;
   };
+
   return (
     <div>
       <PostView post={postInfo} />
