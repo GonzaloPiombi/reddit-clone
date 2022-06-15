4df67507840eaba42505395a506cc5d9c973c00f
Gonzalo <gonzalopiombi@gmail.com>
2022-06-15 17:59:54 -0300

    Create reusable component with basic styles

---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+  margin: 0 auto;
+  position: relative;
+  width: 650px;
+`;
+
+export default Container;
