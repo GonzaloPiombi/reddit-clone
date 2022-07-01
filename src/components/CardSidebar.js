e27a100a6089722ea3aec626360c2bfb020e4a87
Gonzalo <gonzalopiombi@gmail.com>
2022-07-01 16:29:51 -0300

    When the component renders it fetches the upvotes and downvotes made by the user

--- src/components/CardSidebar.js
+++ src/components/CardSidebar.js
@@ -2,23 +2,53 @@ import { StyledCardSidebar } from './styles/Card.styled';
 import { useAuth } from '../AuthContext';
 import { vote } from '../helpers/helpers';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { collection, getDocs, getFirestore } from '@firebase/firestore';
 
 const CardSidebar = ({ votes, path }) => {
-  const [currentVote, setVote] = useState(0);
   const { currentUser } = useAuth();
+  const [currentVotes, setVotes] = useState(() => votes);
+  const [upvote, toggleUpvote] = useState();
+  const [downvote, toggleDownvote] = useState();
 
-  const handleClick = async (e, value) => {
+  useEffect(() => {
+    const getVote = async () => {
+      try {
+        console.log(votes, path);
+        const db = getFirestore();
+        const colRef = collection(db, path, 'votes');
+        const snapshot = await getDocs(colRef);
+        const isVoted = snapshot.docs.find(
+          (doc) => doc.data().uid === currentUser.uid
+        );
+        if (!isVoted) {
+          toggleUpvote(false);
+          toggleDownvote(false);
+        } else if (isVoted.data().vote === 1) {
+          toggleUpvote(true);
+        } else {
+          toggleDownvote(true);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getVote();
+  }, [currentUser]);
+
+  const handleClick = async (value) => {
     try {
-      const button = e.currentTarget;
       const newValue = await vote(path, currentUser.uid, value);
       if (value === 1) {
-        button.classList.toggle('up');
-        button.parentNode.childNodes[2].classList.remove('down');
+        toggleUpvote(!upvote);
+        toggleDownvote(false);
       } else {
-        button.classList.toggle('down');
-        button.parentNode.childNodes[0].classList.remove('up');
+        toggleDownvote(!downvote);
+        toggleUpvote(false);
       }
-      setVote(() => newValue);
+      console.log(newValue);
+      setVotes((prevValue) => prevValue + newValue);
     } catch (error) {
       console.log(error.message);
     }
@@ -26,11 +56,14 @@ const CardSidebar = ({ votes, path }) => {
 
   return (
     <StyledCardSidebar>
-      <button onClick={(e) => handleClick(e, 1)}>
+      <button className={upvote ? 'up' : null} onClick={() => handleClick(1)}>
         <i className="las la-caret-up"></i>
       </button>
-      <p>{votes + currentVote}</p>
-      <button onClick={(e) => handleClick(e, -1)}>
+      <p>{currentVotes}</p>
+      <button
+        className={downvote ? 'down' : null}
+        onClick={() => handleClick(-1)}
+      >
         <i className="las la-caret-down"></i>
       </button>
     </StyledCardSidebar>
