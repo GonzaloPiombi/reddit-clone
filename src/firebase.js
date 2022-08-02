e4ba4b20514f536ac4b92fec65e7cbe19ee72962
Gonzalo <gonzalopiombi@gmail.com>
2022-08-02 13:56:00 -0300

    Create environmental variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBBOAVqDHzUpd4aYIJDHo4GMRy5XW_kU5w',
-  authDomain: 'reddit-clone-ccda2.firebaseapp.com',
-  projectId: 'reddit-clone-ccda2',
-  storageBucket: 'reddit-clone-ccda2.appspot.com',
-  messagingSenderId: '313803069299',
-  appId: '1:313803069299:web:e780770bf4cfd920c41706',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 function firebase() {
